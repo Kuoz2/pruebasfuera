f1253f86c21d526a009ccb0c0a3508f5938f5d5d
KuozDavid <david.palta.anes1989@gmail.com>
2020-05-31 16:38:00 -0400

    otra cosa

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/multikart-admin'));
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + 'dist/multikart-admin/index.html'))
+    res.sendFile(path.join(__dirname + 'dist/multikart-admin/index.html'));
 })
