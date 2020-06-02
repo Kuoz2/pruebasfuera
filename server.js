4e5e9a2a7eea82389965d6284f9edd68eac5bf7a
KuozDavid <david.palta.anes1989@gmail.com>
2020-06-01 22:20:23 -0400

    por fin funciona :D5

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express');
-cost path = require('path');
-
-const app = express();
-
-app.use(express.static(__dirname + '/dist/multikart-admin'));
-app.get('/', function (req, res) => {
-    res.status(200).sendFile(path.join(__dirname + 'dist/multikart-admin/index.html'));
+const app =  express();
+app.use(express.static(__dirname + '/dist/multikart-admin/'));
+app.all('*' (req, res)=>{
+    res.status(200).sendFile(__dirname + '/dist/multikart-admin/index.html');
 });
 
 app.listen(process.env.PORT || 8080)
