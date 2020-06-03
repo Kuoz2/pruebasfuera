4dfa8d839b158bbae921111d09e97075c3e9fe4c
KuozDavid <david.palta.anes1989@gmail.com>
2020-06-03 15:04:08 -0400

    esto resultara

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app =  express();
-app.use(express.static(__dirname + '/dist/multikart-admin/'));
-app.all('*' (req, res)=>{
-    res.status(200).sendFile(__dirname + '/dist/multikart-admin/index.html');
-});
-
-app.listen(process.env.PORT || 8080)
