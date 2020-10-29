d6bc2248b7525b4113b00da6261d8f0699053a32
KuozDavid <david.palta.anes1989@gmail.com>
2020-10-29 14:20:54 -0300

    cambiado cosas

--- src/app/components/pages/appsale/appsale.component.ts
+++ src/app/components/pages/appsale/appsale.component.ts
@@ -96,12 +96,18 @@ export class AppsaleComponent implements OnInit {
 
   imprimir(register){
 
-    var ficha = document.getElementById(register)
-    var vetimp = window.open( '','PRINT', 'height=400,width=600')
+    var ficha = window.document.getElementById(register);
+    var vetimp = window.open( '','PRINT', 'height=400,width=600');
+    vetimp.document.write('<html><head><title>' + document.title  + '</title>');
+    vetimp.document.write('</head><body >');
+    vetimp.document.write('<h1>' + document.title  + '</h1>');
     vetimp.document.write(ficha.innerHTML);
-    vetimp.document.close()
-    vetimp.print()
+    vetimp.document.write('</body></html>');
+    vetimp.document.close();
+    vetimp.focus();
+    vetimp.print();
     vetimp.close()
+    return true
   }
 
 
