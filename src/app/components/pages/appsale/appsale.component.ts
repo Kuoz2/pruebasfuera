2a0148e15df0d429c09e244f3e16c3340a6058a9
KuozDavid <david.palta.anes1989@gmail.com>
2020-10-30 21:37:28 -0300

    cambiado cosas

--- src/app/components/pages/appsale/appsale.component.ts
+++ src/app/components/pages/appsale/appsale.component.ts
@@ -98,16 +98,14 @@ export class AppsaleComponent implements OnInit {
    imprimir(register):Boolean{
   try {
     var mywindow = window.open( '', 'my div', 'height=600,width=1000' );
-    mywindow.document.write( '<html moznomarginboxes mozdisallowselectionprint lang="ES"><head><title>Sticker #1</title>' );
+    mywindow.document.write( '<html lang="ES"><head><title>Sticker #1</title>' );
     mywindow.document.write( '</head><body>' );
     mywindow.document.write( document.getElementById( register ).innerHTML.trim() );
     mywindow.document.write( '</body></html>' );
     mywindow.document.close(); // necessary for IE >= 10
-    mywindow.focus(); // necessary for IE >= 10
-    setTimeout( function () {
       mywindow.print();
       mywindow.close();
-    }, 500 );
+
   } catch (ex) {
     alert( 'Hubo un error al imprimir. Intente de nuevo.' );
     console.log( ex );
