b07456728c73eaf69ffeebfb5a65a39e69b551df
KuozDavid <david.palta.anes1989@gmail.com>
2020-11-02 00:30:11 -0300

    cambiado cosas

--- src/app/components/pages/appsale/appsale.component.ts
+++ src/app/components/pages/appsale/appsale.component.ts
@@ -41,6 +41,9 @@ export class AppsaleComponent implements OnInit {
   //Variable para el formulario
   app_venta: FormGroup
   public categorias: Categories[];
+  private printWindowSubscription;
+
+
 
   constructor(private carservice:CartServiceService,
               private sermedio:PagosService,
@@ -113,9 +116,67 @@ export class AppsaleComponent implements OnInit {
     alert( 'Hubo un error al imprimir. Intente de nuevo.' );
     console.log( ex );
   }*/
+   //  this.printWindowSubscription = this.printer.$printWindowOpen.subscribe(val => {
+     //  console.log('Print window is open:', val);
+     //});
 
- this.printer.printDiv(register)
+ //this.printer.printDiv('ticket')
+     var mywindow = window;
+     //mywindow.document.write( document.getElementById( 'ticket' ).innerHTML.trim() );
 
+     // language=HTML
+     mywindow.document.write(
+         '<head>'+
+         '<style type="text/css">' +
+         '@page  {size: auto; margin: 0}' +
+         'header,footer,aside{display: none }' +
+         '</style>' +
+         '<title></title></head>'+
+         '<div style="font-size: xx-small;  text-align: center;"> '
+         +'<div style="border: solid blue">' +
+         '<div style="border: solid yellow;" >' +
+         'Botillería Poma v10' +
+         '<br> Calama, Av Huaytiquina #947' +
+         ' <br>Rut:' +
+         '<br>BOLETA ELECTRONICA No.' +
+         '<br>23/08/2017 08:22 a.m.' +
+         '</div>' +
+         '<table style=" margin: 0  auto;">' +
+         '<thead style="font-size: xx-small; " >' +
+         '<tr>' +
+         '<th >IVA</th>' +
+         '<th >Producto</th>' +
+         '<th >$$</th>' +
+         '</tr>' +
+         '</thead>' +
+         '<tbody style="font-size: xx-small;" >' +
+         '<tr>' +
+         '<td >1.00</td>' +
+         '<td>Cristal</td>' +
+         '<td>$1000</td>' +
+         '</tr>' +
+         '<tr>' +
+         '<td>2.00</td>' +
+         '<td>Royal</td>' +
+         '<td>$4000</td>' +
+         '</tr>' +
+         '<tr>' +
+         '<td>3.00</td>' +
+         '<td>Baltica</td>' +
+         '<td>$5000</td>' +
+         '</tr>' +
+         '</tbody>' +
+         '</table>'
+         +'</div>'
+         +'</div>'
+
+     )
+
+
+     setTimeout( function () {
+
+       mywindow.print()
+     }, 100)
   }
 
 
--- src/app/components/pages/boleta/boleta.component.ts
+++ src/app/components/pages/boleta/boleta.component.ts
@@ -12,4 +12,64 @@ export class BoletaComponent implements OnInit {
   ngOnInit(): void {
   }
 
+    imprimir() {
+      var mywindow = window;
+      //mywindow.document.write( document.getElementById( 'ticket' ).innerHTML.trim() );
+
+        // language=HTML
+      mywindow.document.write(
+            '<head>'+
+            '<style type="text/css">' +
+            '@page  {size: auto; margin: 0}' +
+            'header,footer,aside{display: none }' +
+            '</style>' +
+            '<title></title></head>'+
+            '<div style="font-size: xx-small; border: solid red; text-align: center;"> '
+            +'<div style="border: solid blue">' +
+            '<div style="border: solid yellow;" >' +
+            'Botillería Poma v10' +
+            '<br> Calama, Av Huaytiquina #947' +
+            ' <br>Rut:' +
+            '<br>BOLETA ELECTRONICA No.' +
+            '<br>23/08/2017 08:22 a.m.' +
+            '</div>' +
+            '<table style="border: solid; margin: 0 auto;">' +
+            '<thead style="font-size: xx-small; " style="border: solid">' +
+            '<tr>' +
+            '<th >IVA</th>' +
+            '<th >Producto</th>' +
+            '<th >$$</th>' +
+            '</tr>' +
+            '</thead>' +
+            '<tbody style="font-size: xx-small; style="border: solid"" >' +
+            '<tr>' +
+            '<td >1.00</td>' +
+            '<td>Cristal</td>' +
+            '<td>$1000</td>' +
+            '</tr>' +
+            '<tr>' +
+            '<td>2.00</td>' +
+            '<td>Royal</td>' +
+            '<td>$4000</td>' +
+            '</tr>' +
+            '<tr>' +
+            '<td>3.00</td>' +
+            '<td>Baltica</td>' +
+            '<td>$5000</td>' +
+            '</tr>' +
+            '</tbody>' +
+            '</table>'
+            +'</div>'
+            +'</div>'
+
+            )
+
+
+      setTimeout( function () {
+
+        mywindow.print()
+      }, 100)
+
+        return false
+    }
 }
