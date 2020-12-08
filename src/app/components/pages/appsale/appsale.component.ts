0fd5ddc18335054a4e6091ec7b853aede8209c2f
KuozDavid <david.palta.anes1989@gmail.com>
2020-12-08 00:07:17 -0300

    probandoinstalacion

--- src/app/components/pages/appsale/appsale.component.ts
+++ src/app/components/pages/appsale/appsale.component.ts
@@ -147,7 +147,7 @@ imagenjpg
            '}'+
            '\n' +
            'img {\n' +
-           'width: 120mm;\n' +
+           'width: 90mm;\n' +
            '  height: 500px;' +
            '}' +
            '</style>' +
