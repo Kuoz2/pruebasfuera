483d75da7f2acbacdaef82084e279a01a25f6b79
KuozDavid <david.palta.anes1989@gmail.com>
2021-08-10 21:57:30 -0400

    no stengo ida

--- src/app/Service/autentificacion.service.ts
+++ src/app/Service/autentificacion.service.ts
@@ -51,7 +51,7 @@ export class AutentificacionService {
             }))
             .subscribe( (res) => {
                 console.log(res)
-                const token = res.headers.get('authorization').replace('Bearer', '');
+                const token = res.headers.get('authorization')
                 console.log(token)
                 if (token == null) {
                     alert( 'No tiene permiso para ingresar.' );
