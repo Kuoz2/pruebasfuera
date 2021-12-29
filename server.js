ff64036c8ef3504b506de080438c7b38f9d7e19e
Kuoz2 <david.palta.anes1989@gmail.com>
2021-12-28 23:33:51 -0300

    dasdas

--- server.js
+++ server.js
@@ -66,18 +66,7 @@ app.listen(process.env.PORT || 8080, () => {
 });
 */
 
-const PORT = process.env.PORT || 5000;
-const INDEX = '/index.html';
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-  const io = socketIO(server);
-  io.on('connection', (socket) => {
-    console.log('Client connected');
-    socket.on('disconnect', () => console.log('Client disconnected'));
-  });
 
   app.listen(process.env.PORT || 8080, () => {
 
