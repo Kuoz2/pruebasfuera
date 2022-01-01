98785bf6e59d9c73a9efcc3ce0b16697a3e410d4
Kuoz2 <david.palta.anes1989@gmail.com>
2022-01-01 18:48:11 -0300

    ndasjdas

--- src/app/components/products/physical/factura/factura.component.ts
+++ src/app/components/products/physical/factura/factura.component.ts
@@ -1,4 +1,7 @@
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-factura',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./factura.component.scss']
 })
 export class FacturaComponent implements OnInit {
+  public closeResult: string;
+  public cotizacion: FormGroup
+  constructor(private modalService: NgbModal, private frm: FormBuilder) { 
+this.cotizacion = frm.group({
+  cliente: new FormControl(''),
+  telefono: new FormControl(''),
+  corre: new FormControl(''),
+  fecha: new FormControl(''),
+  descuento: new FormControl(''),
+  total: new FormControl('')
+})
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
-
+  open(content) {
+    console.log(content)
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', size: <any>'xl ' }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+getDismissReason(reason){
+  if (reason === ModalDismissReasons.ESC) {
+    return 'by pressing ESC';
+  } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+    return 'by clicking on a backdrop';
+  } else {
+    return `with: ${reason}`;
+  }
+  }
 }
