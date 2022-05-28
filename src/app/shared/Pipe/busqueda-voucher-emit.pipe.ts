9ca04b872183686028e8da936a14d95c60a6e18e
Kuoz2 <34548898+Kuoz2@users.noreply.github.com>
2022-05-27 21:50:39 -0500

    subienod cosas

--- src/app/shared/Pipe/busqueda-voucher-emit.pipe.ts
+++ src/app/shared/Pipe/busqueda-voucher-emit.pipe.ts
@@ -16,20 +16,7 @@ export class BusquedaVoucherEmitPipe implements PipeTransform {
 
       for (const p in value) {
         if (value[p].cod_market.toString().indexOf( args ) > -1 || value[p].cod_panaderia.toString().indexOf( args ) > -1 && value[p].cod_market == args || value[p].cod_panaderia == args) {
-          if(value[p].cod_market.toString().indexOf( args ) > -1 && value[p].cod_panaderia.toString().indexOf( args ) > -1 ){
-            voucherencontrado.push( value[p] )
-            break
-          }
-          if(value[p].cod_market =! 0 && value[p].cod_panaderia == 0){
-            voucherencontrado.push( value[p] )
-            break
-          }
-          if(value[p].cod_panaderia != 0 && value[p].cod_market == 0){
-            voucherencontrado.push( value[p] )
-            break
-          }
-        
-
+          voucherencontrado.push(value[p])
         }
       }
       return voucherencontrado
